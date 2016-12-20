e58afcad0df1588696735e7c087c8bab61c7cb00
sushseqi <sushseqi@gmail.com>
2016-12-20 16:13:11 +0100

    Update 2_Battle(winamax_battle).java

--- 2_Battle(winamax_battle).java
+++ 2_Battle(winamax_battle).java
@@ -19,7 +19,9 @@ class Solution {
         Queue playerM = new LinkedList();
         int round = 0;
         int winnerNumner = 0;
-        
+        int playerNRouds = 0;
+        int playerMRouds = 0;
+        boolean pat = false;
         //creation of the list of all cards
         Map<String, Integer> refCards = new HashMap<String, Integer>();
          for (int i = 0; i < 8; i++) {
@@ -51,31 +53,37 @@ class Solution {
         while((playerN.size() > 0) && (playerM.size() > 0)){
             String cardpN = playerN.remove().toString();
             String cardpM = playerM.remove().toString();
-            Queue winner = playerN;
+            
+            Queue winner = null;
             winnerNumner = 0;
             warCards = new String[8];
             tmpWarCards= new ArrayList<String>();
             
+            //get players' card numbers
             int indexN = refCards.get(cardpN);
             int indexM = refCards.get(cardpM);
             
+            // if numbers are not equals there is a winner for this round
             if(indexN != indexM){
                 
                 if(indexN < indexM){
                    winner =  playerM;
                    winnerNumner = 2;
+                   playerMRouds++;
                 }else if (indexN > indexM){
                    winner =  playerN;
                    winnerNumner = 1; 
+                   playerNRouds++;
                 }
-                
+                System.err.println("Round : " + round + "; P1 : " + playerNRouds +" <-> P2 : " +playerMRouds+ " (" + cardpN + " - " + cardpM + ")->" + winnerNumner );
+
                 int longeur = refWarCards.size(); // number of war cards 
                 if (longeur > 0){
-                  for(int i = 0; i < longeur; i++){
-                      if( i == longeur/2 - 1){
+                    for(int i = 0; i < longeur; i++){
+                        if( i == longeur/2 - 1){
                           winner.add(cardpN);
-                      }
-                    winner.add(refWarCards.get(i));
+                        } 
+                        winner.add(refWarCards.get(i));
                     }  
                 }
                 winner.add(cardpM);
@@ -85,39 +93,41 @@ class Solution {
                 //war card of current round to save in array warCards
                 warCards[0] = cardpN; 
                 warCards[4] = cardpM; 
+                
                 for(int i = 1; i<4; i++) {
-                   warCards[i] = playerN.remove().toString(); 
-                   warCards[i+4] = playerM.remove().toString();
-                }
-                System.err.println("War Cards of current round : ");
-                for(int i = 1; i<warCards.length; i++) {
-                   System.err.println(warCards[i]);
-                }
-                // war  cards remaining of previous rounds
-                // cards of first player
-                if(refWarCards != null){
-                      for (int k = 0; k < refWarCards.size(); k++){
-                        if(k == refWarCards.size()/2){
-                            for(int j = 0; j < warCards.length / 2; j++){
-                                tmpWarCards.add(warCards[j]); // first half of war cards of current round corresponding to first player
-                            }
-                            }else{
-                                tmpWarCards.add(refWarCards.get(k)); // war cards of previous rounds 
-                            }
-                           
-                        } 
-                        for(int j = warCards.length/2; j < warCards.length; j++){
-                           tmpWarCards.add(warCards[j]);// second half of war cards of current round corresponding to second player
-                       } 
-                } else{
-                    for(int j = 0; j < warCards.length; j++){
-                        tmpWarCards.add(warCards[j]); // first half of war cards of current round corresponding to first player
+                    if((playerN.size() > 0) && (playerM.size() > 0)){
+                       warCards[i] = playerN.remove().toString(); 
+                       warCards[i+4] = playerM.remove().toString();
+                    } else{
+                        pat = true;
+                        break;
                     }
                 }
+                System.err.println("War Cards of current round : ");
+                for(int i = 0; i<warCards.length; i++) {
+                   System.err.println(warCards[i]);
+                }
+                System.err.println("+++++++++++++++++++++++");
+                
+                // war  cards remaining of previous rounds
+                // cards of first player
+                for (int k = 0; k < refWarCards.size(); k++){
+                    if(k == refWarCards.size()/2){
+                        for(int j = 0; j < warCards.length / 2; j++){
+                            tmpWarCards.add(warCards[j]); // first half of war cards of current round corresponding to first player
+                        }
+                    }else{
+                        tmpWarCards.add(refWarCards.get(k)); // war cards of previous rounds 
+                    }
+                   
+                } 
+                for(int j = warCards.length/2; j < warCards.length; j++){
+                   tmpWarCards.add(warCards[j]);// second half of war cards of current round corresponding to second player
+               } 
                refWarCards = tmpWarCards;
             }
-        } if(winnerNumner > 0){
-            System.out.println(winnerNumner + " " + round);
+        } if(!pat){
+            System.out.println(((playerNRouds > playerMRouds)? "1" : "2") + " " + round);
         }else{
             System.out.println("PAT");
         }
